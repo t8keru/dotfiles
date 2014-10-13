d3f5742e11ee5f0cb835625d5b2dc8d37234fc55
Takeru Sato <midium.size@gmail.com>
2014-10-13 12:57:14 +0900

    Update init.el

--- init.el
+++ init.el
@@ -45,6 +45,7 @@
 ;; @ Helm
 (when (require 'helm-config nil t)
   (helm-mode 1)
+  (require 'helm-files)
 
   (define-key global-map (kbd "C-x C-f") 'helm-for-files)
   (define-key global-map (kbd "M-x") 'helm-M-x)
@@ -82,7 +83,46 @@
                         ;; '^' is a pattern for basename
                         ;; and not required because the directory name is prepended
                         (substring input-pattern 1)
-                      (concat ".*" input-pattern)))))))
+                      (concat ".*" input-pattern))))))
+
+  ;; List files in git repos
+  ;; From @ http://d.hatena.ne.jp/syohex/20120718/1342620627 >>>
+  (defun helm-c-sources-git-project-for (pwd)
+    (loop for elt in
+          '(("Modified files" . "--modified")
+            ("Untracked files" . "--others --exclude-standard")
+            ("All controlled files in this project" . nil))
+          for title  = (format "%s (%s)" (car elt) pwd)
+          for option = (cdr elt)
+          for cmd    = (format "git ls-files %s" (or option ""))
+          collect
+          `((name . ,title)
+            (init . (lambda ()
+                      (unless (and (not ,option) (helm-candidate-buffer))
+                        (with-current-buffer (helm-candidate-buffer 'global)
+                          (call-process-shell-command ,cmd nil t nil)))))
+            (candidates-in-buffer)
+            (type . file))))
+
+  (defun helm-git-project-topdir ()
+    (file-name-as-directory
+     (replace-regexp-in-string
+      "\n" ""
+      (shell-command-to-string "git rev-parse --show-toplevel"))))
+
+  (defun helm-git-project ()
+    (interactive)
+    (let ((topdir (helm-git-project-topdir)))
+      (unless (file-directory-p topdir)
+        (error "I'm not in Git Repository!!"))
+      (let* ((default-directory topdir)
+             (sources (helm-c-sources-git-project-for default-directory)))
+        (helm-other-buffer sources
+                           (format "*helm git project in %s*" default-directory)))))
+  (define-key global-map (kbd "C-;") 'helm-git-project)
+  ;; <<<
+
+  )
 
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 
