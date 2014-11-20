cd6b8734e155d281973e17d978c05f8fbb2f5542
Takeru Sato <midium.size@gmail.com>
2014-11-20 22:31:17 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 case $(uname) in
-"FreeBSD") pkg install -y gcc49 openjdk8 ruby python mercurial git vim gvim hs-haskell-platform bash gmake ;;
+"FreeBSD")
+  pkg install -y gcc5 openjdk8 ruby python mercurial git vim gvim hs-haskell-platform bash gmake
+  X=`which gcc5`
+  [ -s "$x" ] && ln -s $x /usr/local/bin/gcc
+  ;;
 *) ;;
 esac
