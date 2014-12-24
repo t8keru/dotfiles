f7aad247a3ff34f0e17ba6caa803914107786f45
t8keru <midium.size@gmail.com>
2014-12-24 22:15:49 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,49 +18,39 @@ cd $ROOT_DIR && git pull --rebase
 # --------------------------------------------------
 # @ zsh
 zsh/setup.sh
-exec $SHELL
 
 # --------------------------------------------------
 # @ emacs
 emacs/setup.sh
-exec $SHELL
 
 # --------------------------------------------------
 # @ haskell
 haskell/setup.sh
-exec $SHELL
 
 # --------------------------------------------------
 # @ golang
 golang/setup.sh
-exec $SHELL
 
 # --------------------------------------------------
 # @ ruby
 ruby/setup.sh 2.1
-exec $SHELL
 
 # --------------------------------------------------
 # @ python
 python/setup.sh 2.7.8
-exec $SHELL
 
 # --------------------------------------------------
 # @ node.js
 nodejs/setup.sh 0.10.33
-exec $SHELL
 
 # --------------------------------------------------
 # @ vim
 vim/setup.sh
-exec $SHELL
 
 # --------------------------------------------------
 # @ nvim
 nvim/setup.sh
-exec $SHELL
 
 # --------------------------------------------------
 # @ git
 git/setup.sh
-exec $SHELL
