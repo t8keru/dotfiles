862676132d24c10bdffb923ba8c7a4b6dd51bfc0
Takeru Sato <midium.size#h@gmail.com>
2015-01-01 23:12:22 +0900

    update

--- git/setup.sh
+++ git/setup.sh
@@ -4,4 +4,7 @@ set -ex
 
 cd $(dirname $0)
 
-cp -p gitconfig $HOME/.gitconfig
+git config --global user.name "Takeru Sato"
+git config --global user.email "midium.size#h@gmail.com"
+git config --global ghq.root $HOME/dev/src
+git config --global push.default matching
