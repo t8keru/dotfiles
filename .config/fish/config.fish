4f89e3e39f89469a43f09af0d149b45ed72924d0
Takeru Sato <midium.size@gmail.com>
2016-04-24 21:16:25 +0900

    Add some files

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/taker/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/Users/taker/.config/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
+set fish_path $HOME/.oh-my-fish
+
+set fish_theme lambda
+
+set fish_plugins theme
